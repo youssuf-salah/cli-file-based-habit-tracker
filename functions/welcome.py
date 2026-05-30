cfdf9eb09662515f3409c263f5a65c7b3b68f7a5
youssuf-salah <youssufsalah@outlook.com>
2026-05-31 00:26:07 +0300

    Initialized functions folder as modules and create welcome function

---
+++ functions/welcome.py
@@ -0,0 +1,3 @@
+def welcome() -> None:
+  print("Welcome to the File-Based CLI Habit Tracker!")
+  print("Before you start using the app, please provide pathes to habits.csv and streaks.csv files so you don't lose any progress!")
